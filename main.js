2ae9ff7b47cc5bee809bf603b21e5cdda0624be7
kamela williamson <kami63116@yahoo.com>
2022-06-09 15:11:31 -0700

    adding testing to main.js file

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 //https://jasmine.github.io/pages/docs_home.html
 
+//describe block & a grouping
+describe('Test Example', () => {
+    // it test 
+    it('returns true', () => {
+        // an expectation. need to know what to expect
+        expect(false).toBe();  //toBe is one of the matchers
+    })
+});
+
