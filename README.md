a7d4446abaa206e368ba7f728f3c9f0ce1609e4b
kamela williamson <kami63116@yahoo.com>
2022-06-09 17:24:54 -0700

    added test to main js and commented out example test

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Trying basic unit testing with Jasmine
 
 ![test1](https://user-images.githubusercontent.com/24884380/172953865-61abc4bb-cd1b-422e-9736-a592638044ff.jpg)
 
+
+
--- main.js
+++ main.js
@@ -1,11 +1,38 @@
 //https://jasmine.github.io/pages/docs_home.html
 
 //describe block & a grouping
-describe('Test Example', () => {
-    // it test 
-    it('returns true', () => {
-        // an expectation. need to know what to expect
-        expect(false).toBe();  //toBe is one of the matchers
-    })
+// describe('Test Example', () => {
+//     // it test 
+//     it('returns true', () => {
+//         // an expectation. need to know what to expect
+//         expect(false).toBe(true);  //toBe is one of the matchers
+//     })
+// });
+
+
+function insertDashes(str) {
+    //write code here
+}
+
+
+
+
+/* TEST SUITE */
+
+describe('insertDashes()', () => {
+    it('insert dashes in between chars', () => {
+        //arrange
+        const value = "aba caba";
+
+        //act
+        const result = insertDashes(value);
+
+        //log
+        console.log("result: ", result);
+
+        //assert
+        expect(result).toBe("a-b-a c-a-b-a");
+    });
 });
+
 
